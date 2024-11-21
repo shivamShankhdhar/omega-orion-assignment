ec3d7e4d841259faff493f649d05abc1d284a720
shivam shankhdhar <39639414+shivamShankhdhar@users.noreply.github.com>
2024-11-21 17:20:05 +0530

    Update commentSection.tsx

--- app/components/CommentsSection/commentSection.tsx
+++ app/components/CommentsSection/commentSection.tsx
@@ -5,10 +5,13 @@ import CommentInput from './CommentInput'
 
 const CommentSection = () => {
   return (
-    <div className='flex flex-col gap-2 w-full p-5 bg-black rounded-lg'>
-        <h1 className='font-2xl'>Comments</h1>
+    <div className='w-[90%] flex flex-col gap-2  bg-[#141414] overflow-hidden rounded-lg mb-5'>
+        
+        <div className="w-full pt-10 pl-10 pr-10 pb-0 flex flex-col gap-5">
+        <h1 className='font-2xl font-bold'>Comments</h1>
         <CommentInfo/>
         <Comment/>
+        </div>        
         <CommentInput/>
     </div>
   )
