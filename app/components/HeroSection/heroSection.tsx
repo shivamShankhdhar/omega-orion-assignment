ba0d66a992f43fd3e345b47f89b29d355f130336
shivam shankhdhar <39639414+shivamShankhdhar@users.noreply.github.com>
2024-11-21 12:51:09 +0530

    Update heroSection.tsx

--- app/components/HeroSection/heroSection.tsx
+++ app/components/HeroSection/heroSection.tsx
@@ -9,10 +9,10 @@ const HeroSection = () => {
         <Navbar/>
         <div className="h-[100px]"></div>
         <div className="w-full flex flex-wrap py-3 items-center">
-            <div className="flex-1 ml-[80px] mr-[50px] flex text-2xl font-bold">
+            <div className="flex-1 ml-[80px] mr-[50px] sm:justify-center sm:items-center flex text-2xl font-bold">
                 Pixel Bloom: AI website agency
             </div>
-            <div className="flex gap-5 justify-center items-center px-5 text-sm">
+            <div className="flex gap-5 justify-center items-center px-5 text-sm sm:w-full flex-wrap md:justify-end lg:justify-end sm:justify-center">
                 <div className="w-[fit-content] border border-white py-1 justify-center items-center cursor-pointer  rounded-lg px-3 flex gap-2"><MdOutlineFeedback/> Give feedback</div>
                 <div className="w-[fit-content] border bg-[#cbff00] text-black border-[#cbff00] py-1  justify-center items-center cursor-pointer px-3 rounded-lg">add to cart $20</div>
             </div>
